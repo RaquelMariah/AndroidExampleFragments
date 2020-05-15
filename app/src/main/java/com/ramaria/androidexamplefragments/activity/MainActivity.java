ff5eb4a49b3d5ef2d8367bcb9f9ecd6eed797e9c
Raquel Maria <raquel.maria@vr.com.br>
2020-05-14 22:33:48 -0300

    Adicionados métodos fragment

--- app/src/main/java/com/ramaria/androidexamplefragments/activity/MainActivity.java
+++ app/src/main/java/com/ramaria/androidexamplefragments/activity/MainActivity.java
@@ -1,12 +1,19 @@
 package com.ramaria.androidexamplefragments.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.ramaria.androidexamplefragments.R;
+import com.ramaria.androidexamplefragments.fragments.ConversasFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonConversas;
+    private Button buttonContatos;
+    private ConversasFragment conversasFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setElevation(0);
+
+        conversasFragment = new ConversasFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frameConteudo, conversasFragment);
+        transaction.commit();
+        
 
     }
 }
